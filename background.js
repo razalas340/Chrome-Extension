986eb34c24d7b3a1dba6a89bf3856347d9f7eaea
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-07-10 10:31:26 -0400

    active tab instead of background v3

--- background.js
+++ background.js
@@ -1,13 +1,9 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(
-      tab.id,
-      { code: `
-        const gradeInputs = document.querySelectorAll(".u7S8tc .ksaOtd");
-        // rest of your content script code...
-      ` },
-      function (result) {
-        // Handle the response from the content script, if needed
-        // ...
-      }
-    );
+chrome.action.onClicked.addListener(function(tab) {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['script.js']
+    }, function (result) {
+      // Handle the response from the content script, if needed
+      // ...
+    });
   });
